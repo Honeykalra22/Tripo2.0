8e7a9a04886cc8256b4b20be3733e91d482feeb9
Jujhar Singh Sandhu <sjujhar532@gmail.com>
2024-12-28 19:06:08 +0530

    Add Prettier configuration, cookie-parser dependency, and enhance user registration and logout functionality

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,6 +12,9 @@ app.use(
 )
 app.use(express.json({ limit: '128kb' }))
 app.use(express.urlencoded({ extended: true, limit: '128kb' }))
+app.use(express.static("public"))
+app.use(cookieParser())
+
 
 import userRouter from './routes/user.route.js'
 app.use('/api/v2/user', userRouter)
