114204ce9f5a161280343ffdfc5882b3a82f96ff
Jujhar Singh Sandhu <sjujhar532@gmail.com>
2024-12-28 23:41:48 +0530

    Fixed a minor typo

--- Backend/src/routes/itinerary.route.js
+++ Backend/src/routes/itinerary.route.js
@@ -4,7 +4,7 @@ import { searchLocation } from "../controller/itinerary.controller.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
 
-const router = .Router();
+const router = Router();
 router.use(upload.none());
 router.use(verifyJwt);
 
