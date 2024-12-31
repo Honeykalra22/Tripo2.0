d22c1d97177baf7986e1f1d5d6b8979606b39955
Jujhar Singh Sandhu <sjujhar532@gmail.com>
2024-12-31 22:55:10 +0530

    Enhance itinerary management by adding get and delete functionality, and validate user and itinerary IDs

--- Backend/src/routes/itinerary.route.js
+++ Backend/src/routes/itinerary.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { updateItinerary, searchLocation, getResultFromChatGPT } from "../controller/itinerary.controller.js";
+import { updateItinerary, searchLocation, getResultFromChatGPT, deleteItinerary, getItinerary, getUserItineraries } from "../controller/itinerary.controller.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
 
@@ -10,5 +10,9 @@ router.use(verifyJwt);
 router.route('/updateItinerary').post(verifyJwt, updateItinerary)
 router.route('/searchLocation').post(verifyJwt, searchLocation)
 router.route('/getResultFromChatGPT').get( getResultFromChatGPT)
+router.route('/').get(verifyJwt, getUserItineraries)
+router.route('/:itineraryId').get(verifyJwt, getItinerary)
+router.route('/:itineraryId').delete(verifyJwt, deleteItinerary)
+
 
 export default router
